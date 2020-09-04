edcdaf1a020c8c4e8a00663045f72978a9a5e22a
ethanwc <ethanwc@u.washington.edu>
2020-09-04 16:53:49 -0700

    sliding tiles, start on animations

--- App.tsx
+++ App.tsx
@@ -1,77 +1,18 @@
 import React, {useRef} from 'react';
-import {Animated, FlatList, StyleSheet, View} from 'react-native';
+import {
+  Animated,
+  FlatList,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Easing,
+} from 'react-native';
 import GestureRecognizer from 'react-native-swipe-gestures';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28b1a',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa972f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3rda1-471f-bd96-145571e329d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471fr-bd96-145571e4d9d72',
-    title: 'Third Item',
-  },
-  {
-    id: 'bd7acbea-c1b1-46c2-caed55-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a64f8-fbd91aaa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd976-1f45571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471d96-1845571ed9d72',
-    title: 'Third Item',
-  },
-  {
-    id: 'bd7acbea-c1b1-46c21-aed5-53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c36-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da41-471bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-3471f-bd96-145571ed72',
-    title: 'Third Item',
-  },
-  {
-    id: 'bd7acbea-c1b1-416c2-ae-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3a68af1c-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a20f-3da-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-1435571edd72',
-    title: 'Third Item',
-  },
-];
 const App = () => {
   interface Element {
     id: String;
@@ -81,144 +22,272 @@ const App = () => {
     moveAnimation: any;
   }
 
-  let state = Array<Element | null>(16).fill(null);
+  const ElementView = (props: any) => {
+    return (
+      <View style={{position: 'absolute'}}>
+        <Animated.View style={[props.item.moveAnimation.getLayout()]}>
+          <View
+            style={{
+              backgroundColor: 'grey',
+              margin: wp('1.2%'),
+              width: wp('22%'),
+              height: wp('22%'),
+            }}
+          />
+        </Animated.View>
+      </View>
+    );
+  };
 
-  const moveAnimation0 = new Animated.ValueXY({
-    x: wp('1.2%') + wp('24.4%') * 2,
-    y: wp('1.2%') + +wp('24.4%') * 3,
-  });
+  const BackgroundTile = () => {
+    return <View style={styles.item} />;
+  };
+
+  let state = Array<Element | null>(16).fill(null);
 
   state[0] = {
     id: 'water',
     combinesWith: ['water', 'ice'],
     value: 2,
+    position: {x: 0, y: 0},
+    moveAnimation: new Animated.ValueXY({
+      x: wp('1.2%') + wp('24.4%') * 0,
+      y: wp('1.2%') + wp('24.4%') * 0,
+    }),
+  };
+  state[3] = {
+    id: 'water',
+    combinesWith: ['water', 'ice'],
+    value: 2,
+    position: {x: 0, y: 1},
+    moveAnimation: new Animated.ValueXY({
+      x: wp('1.2%') + wp('24.4%') * 1,
+      y: wp('1.2%') + wp('24.4%') * 0,
+    }),
+  };
+  state[1] = {
+    id: 'fire',
+    combinesWith: ['fire'],
+    value: 2,
     position: {x: 2, y: 2},
-    moveAnimation: moveAnimation0,
+    moveAnimation: new Animated.ValueXY({
+      x: wp('1.2%') + wp('24.4%') * 2,
+      y: wp('1.2%') + wp('24.4%') * 2,
+    }),
   };
 
-  const RenderItem = (props: any) => {
-    return <View style={styles.item}></View>;
+  state[6] = {
+    id: 'fire',
+    combinesWith: ['fire'],
+    value: 2,
+    position: {x: 3, y: 3},
+    moveAnimation: new Animated.ValueXY({
+      x: wp('1.2%') + wp('24.4%') * 3,
+      y: wp('1.2%') + wp('24.4%') * 3,
+    }),
+  };
+
+  /**
+   * Invert an array - used for vertical reallignment
+   */
+  const invert = (positions: any) => {
+    let result: any[] = [];
+
+    for (let i = 0; i < 4; i++) {
+      let temp11: any = [];
+      for (let j = 0; j < 4; j++) {
+        temp11.push(positions[j][i]);
+      }
+      result.push(temp11);
+    }
+    return result;
   };
 
   /**
    * Determine where to move tiles
    */
-  const calculateMoves = () => {
-    // const swipeDirection = 'LEFT';
-    // let boardState = Array.from(Array(4), () => new Array(4));
-    // state.forEach((item: Element | null) => {
-    //   if (item) {
-    //     boardState[item.position.x][item.position.y] = item;
-    //   }
-    // });
-
-    // let l1: any = [];
-    // let l2: any = [];
-    // let l3: any = [];
-    // let l4: any = [];
-
-    // boardState[0].forEach((item: Element | null, index: number) => {
-    //   if (item) {
-    //     let item2 = item;
-    //     item2.position = {x: 0, y: index};
-    //     item2.animation = Animated.timing(moveAnimation1, {
-    //       toValue: {
-    //         x: wp('1.2%') + wp('24.4%') * index,
-    //         y: wp('1.2%') + wp('24.4%') * index,
-    //       },
-    //       duration: 200,
-    //       useNativeDriver: false,
-    //     });
-
-    //     l1.push(item2);
-    //   }
-    // });
-
-    // l1 = [...l1, ...Array(4 - l1.length).fill(undefined)];
-    // l2 = [...l2, ...Array(4 - l2.length).fill(undefined)];
-    // l3 = [...l3, ...Array(4 - l3.length).fill(undefined)];
-    // l4 = [...l4, ...Array(4 - l4.length).fill(undefined)];
-
-    // // console.error(boardState);
-    // // console.error([l1, l2, l3, l4]);
-    // state = [...l1, ...l2, ...l3, ...l4];
-    state.forEach((item: Element | null, index: number) => {
-      if (item) {
-        animation: Animated.timing(item.moveAnimation, {
-          toValue: {
-            x: wp('1.2%') + wp('24.4%') * 2,
-            y: wp('1.2%') + wp('24.4%') * 0,
-          },
-          duration: 200,
-          useNativeDriver: false,
-        }).start();
+  const calculateMoves = (swipeDirection: string) => {
+    let boardState = Array.from(Array(4), () => new Array(4));
+    state.forEach((item: Element | null) => {
+      if (item !== null) {
+        boardState[item.position.x][item.position.y] = item;
       }
     });
 
-    // switch (swipeDirection) {
-    //   case 'LEFT':
-    // }
-    // calculate new board state, set new animation for the objects then call parallel animate
+    //todo: button on press spring animation
+    let rows = Array.from(Array(4), () => new Array());
+    let columns = Array.from(Array(4), () => new Array());
+
+    boardState.forEach((arr: Array<Element | null>, arrIndex: number) => {
+      for (let r = 0; r < 4; r++) {
+        if (arr[r] !== null && arr[r] !== undefined) {
+          columns[r].push(arr[r]);
+        }
+      }
+    });
+
+    boardState.forEach((arr: Array<Element | null>, arrIndex: number) =>
+      arr.forEach((item: Element | null) => {
+        if (item !== null && item !== undefined) rows[arrIndex].push(item);
+      }),
+    );
+
+    let updatedBoard: Array<Array<Element | null>> = [[]];
+
+    switch (swipeDirection) {
+      case 'DOWN':
+        let downdatedPositions: any = columns.map(
+          (column: Array<Element | null>) => {
+            return [
+              ...column.reverse(),
+              ...Array(4 - column.length).fill(null),
+            ].reverse();
+          },
+        );
+
+        updatedBoard = invert(downdatedPositions);
+        break;
+
+      case 'UP':
+        let updatedPositions: any = columns.map(
+          (column: Array<Element | null>) => {
+            return [...column, ...Array(4 - column.length).fill(null)];
+          },
+        );
+
+        updatedBoard = invert(updatedPositions);
+        break;
+
+      case 'LEFT':
+        updatedBoard = rows.map((row: Array<Array<Element | null>>) => {
+          return [...row, ...Array(4 - row.length).fill(null)];
+        });
+        break;
+      case 'RIGHT':
+        updatedBoard = rows.map((row: Array<Array<Element | null>>) => {
+          return [
+            ...row.reverse(),
+            ...Array(4 - row.length).fill(null),
+          ].reverse();
+        });
+        break;
+    }
+
+    if (updatedBoard !== null) {
+      for (let y = 0; y < updatedBoard.length; y++) {
+        for (let x = 0; x < updatedBoard.length; x++) {
+          if (updatedBoard[x][y] !== null && updatedBoard[x][y] !== undefined) {
+            updatedBoard[x][y].position = {x: x, y: y};
+          }
+        }
+      }
+    }
+
+    state = [
+      ...updatedBoard[0],
+      ...updatedBoard[1],
+      ...updatedBoard[2],
+      ...updatedBoard[3],
+    ];
+
+    let animationQueue: Animated.CompositeAnimation[] = [];
+
+    state.forEach((item: Element | null, index: number) => {
+      if (item) {
+        animationQueue.push(
+          Animated.timing(item.moveAnimation, {
+            toValue: {
+              x: wp('1.2%') + wp('24.4%') * item.position.y,
+              y: wp('1.2%') + wp('24.4%') * item.position.x,
+            },
+            duration: 100,
+            useNativeDriver: false,
+          }),
+        );
+        item.position = {x: item.position.x, y: item.position.y}; // x and y flipped?
+      }
+    });
+
+    // Start all tile moves at the same time
+    Animated.parallel(animationQueue).start();
   };
-
-  const moveAnimation1 = new Animated.ValueXY({
-    x: wp('1.2%') + wp('24.4%') * 1,
-    y: wp('1.2%') + +wp('24.4%') * 1,
-  });
-
-  const moveAnimation2 = new Animated.ValueXY({
-    x: wp('1.2%') + wp('24.4%') * 1,
-    y: wp('1.2%') + +wp('24.4%') * 1,
-  });
 
   const handleSwipe = (direction: string) => {
     switch (direction) {
       case 'SWIPE_LEFT':
+        calculateMoves('LEFT');
         break;
       case 'SWIPE_RIGHT':
-        console.log('swipe right');
-        // if (state[0]) console.log(state[0].animation);
+        calculateMoves('RIGHT');
         break;
       case 'SWIPE_UP':
-        console.log('swipe up');
-        calculateMoves();
+        calculateMoves('UP');
         break;
       case 'SWIPE_DOWN':
-        console.log('swipe down');
+        calculateMoves('DOWN');
         break;
     }
   };
 
+  let width = new Animated.Value(100);
+  const endWidth = 40;
+  let a = Animated.timing(width, {
+    toValue: endWidth,
+    useNativeDriver: false,
+    duration: 200,
+    easing: Easing.linear,
+  });
+  let opacity = new Animated.Value(0);
+
+  const animate = (easing: any) => {
+    opacity.setValue(0);
+    Animated.timing(opacity, {
+      toValue: 1,
+      useNativeDriver: false,
+      duration: 400,
+      easing,
+    }).start();
+  };
+
+  const size = opacity.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 120],
+  });
+  const animatedStyles = [
+    styles.box,
+    {
+      // opacity,
+      width: size,
+      height: size,
+    },
+  ];
   return (
     // Listen for gesture input
-    <GestureRecognizer onSwipe={(direction, state) => handleSwipe(direction)}>
-      <View style={{backgroundColor: 'grey'}}>
-        <View style={styles.container}>
-          {/* Render the game board */}
-          <FlatList
-            data={DATA}
-            numColumns={4}
-            renderItem={({item}) => (
-              <RenderItem id={item.id} title={item.title} />
-            )}
-            keyExtractor={(item) => item.title}
-            decelerationRate={0.798}
-            showsHorizontalScrollIndicator={false}
-          />
-          {/* Render the tiles */}
-          <View style={{position: 'absolute'}}>
-            <Animated.View style={[state[0].moveAnimation.getLayout()]}>
-              <View
-                style={{
-                  backgroundColor: 'grey',
-                  margin: wp('1.2%'),
-                  width: wp('22%'),
-                  height: wp('22%'),
-                }}
-              />
-            </Animated.View>
+    <GestureRecognizer onSwipe={(direction, _) => handleSwipe(direction)}>
+      <View>
+        <View style={{backgroundColor: 'grey'}}>
+          <View style={styles.container}>
+            <FlatList
+              data={Array(16).fill('')}
+              numColumns={4}
+              renderItem={({item}) => <BackgroundTile />}
+              keyExtractor={(item) => item.title}
+              decelerationRate={0.798}
+              showsHorizontalScrollIndicator={false}
+            />
+
+            {state.map((item: Element | null, index: number) => {
+              return item !== null ? <ElementView item={item} /> : null;
+            })}
           </View>
         </View>
+        <TouchableOpacity
+          onPress={() => animate(Easing.elastic(2))}
+          style={{position: 'absolute'}}>
+          <View style={styles.boxContainer}>
+            <Animated.View style={animatedStyles} />
+          </View>
+        </TouchableOpacity>
       </View>
     </GestureRecognizer>
   );
@@ -232,12 +301,14 @@ const styles = StyleSheet.create({
     marginVertical: wp('50%'),
     backgroundColor: '#bbada0',
   },
-  textWrapper: {
-    height: hp('100%'),
-    width: wp('100%'),
+  box: {
+    marginTop: 32,
+    borderRadius: 4,
+    backgroundColor: '#61dafb',
   },
-  myText: {
-    fontSize: hp('5%'),
+  boxContainer: {
+    height: 160,
+    alignItems: 'center',
   },
   item: {
     backgroundColor: '#cdc0b4',
@@ -245,23 +316,6 @@ const styles = StyleSheet.create({
     marginVertical: wp('1.2%'),
     height: wp('22%'),
     width: wp('22%'),
-  },
-  item2: {
-    backgroundColor: 'purple',
-    marginHorizontal: wp('1.2%'),
-    marginVertical: wp('1.2%'),
-    height: wp('22%'),
-    width: wp('22%'),
-    position: 'absolute',
-    transform: [
-      {translateX: wp('1.2%')},
-      {
-        translateY: wp('1.2%'),
-      },
-    ],
-  },
-  title: {
-    fontSize: 32,
   },
 });
 
